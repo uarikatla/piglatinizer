5e48be8c9d15a787fc82732b92e2209920c647dd
uarikatla <ujwala.arikatla@riskified.com>
2019-11-20 14:05:40 +0000

    updated js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#button").click(function(){
 
+  let word = $("#input").val();
+  let vowels = ["a", "e", "i", "o", "u"];
+  $("#output").text("");
 
+  if(word.substring(0, 1) in vowels){
 
-  
-  
-  
-  
-});
+    out_word = word + "ay";
+
+  } else{
+    
+    out_word = word.substring(1) + word.substring(0, 1) + "ay";
+
+  }
+
+  $("#output").text(out_word)
+
+})

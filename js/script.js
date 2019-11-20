506df3964017719c9b1275e575d37d8eb0006fba
uarikatla <ujwala.arikatla@riskified.com>
2019-11-20 14:32:31 +0000

    updated JS

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-$("#button").click(function(){
-
-  let word = $("#input").val();
-  let vowels = ["a", "e", "i", "o", "u"];
-  $("#output").text("");
-
-  if(word.substring(0, 1) in vowels){
-
-    out_word = word + "ay";
-
-  } else{
-    
-    out_word = word.substring(1) + word.substring(0, 1) + "ay";
-
-  }
-
-  $("#output").text(out_word)
-
-})
+$( "document" ).ready(function(){
+$("button").click(function(){
+    let userInput = $("input").val();
+    let splitter = userInput.split(" ");
+    console.log(splitter);
+    splitter.forEach(function(element){
+        let pigLatin = element.charAt(0);
+        let slicer = element.slice(1);
+            if (pigLatin === "a"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }else if (pigLatin === "e"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }else if (pigLatin === "i"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }else if (pigLatin === "o"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }else if (pigLatin === "u"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }
+            else{
+                $(".output").append(` ${slicer}${pigLatin}ay`);
+            }
+    });
+});
+});
